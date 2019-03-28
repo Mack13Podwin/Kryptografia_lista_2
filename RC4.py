9f76f988a4d69b7a8082914bbb51840638cac730
Mack13Podwin <maciej.podwinski@gmail.com>
2019-03-28 13:41:01 +0100

    Adding: hopefuly finishing

--- RC4.py
+++ RC4.py
@@ -5,8 +5,6 @@ import struct
 
 
 class RC4:
-	i = 0
-	j = 0
 
 	def __init__(self, l, n, mode='', t=16):
 		self.length = l
@@ -19,37 +17,38 @@ class RC4:
 		elif mode == 'sst':
 			self.ksa_sst()
 
-	def prga(self):
-		self.i = (self.i+1) % self.n
-		self.j = (self.j+self.s[self.i]) % self.n
-		self.s[self.i], self.s[self.j] = self.s[self.j], self.s[self.i]
-		return self.s[(self.s[self.i]+self.s[self.j]) % self.n]
+	def prga(self, i, j):
+		self.s[i], self.s[j] = self.s[j], self.s[i]
+		return self.s[(self.s[i]+self.s[j]) % self.n]
 
 	def mdrop(self, d):
 		val = 1
+		i, j = 0,0
 		if self.n == 16:
 			val = 2
 		elif self.n == 64:
 			val = 3
-		while True:
+		for z in range(100000000):
+			i = (i+1)% self.n
+			j = (j+self.s[i])% self.n
 			if val==1:
-				for i in range(d):
-					self.prga()
-				stdout.buffer.write(struct.pack('>I', self.prga()))
+				for k in range(d):
+					self.prga(i,j)
+				stdout.buffer.write(bytes([self.prga(i,j)]))
 			elif val==2:
-				for i in range(2*d):
-					self.prga()
-				out = self.prga()<<4 +self.prga()
-				stdout.buffer.write(struct.pack('>I', out))
+				for k in range(2*d):
+					self.prga(i,j)
+				out = (self.prga(i,j)<<4)+self.prga(i,j)
+				stdout.buffer.write(bytes([out]))
 			elif val==3:
 				result = 0
-				for i in range(4*(d+1)):
+				for k in range(4*(d+1)):
 					result=result<<6
-					result+=self.prga()
+					result+=self.prga(i,j)
 				result = list(result.to_bytes(3*(d+1), byteorder='big'))
-				for i in range(3*(d+1)):
-					if i%(d+1)==d:
-						stdout.buffer.write(struct.pack('>I',result[i]))
+				for k in range(3*(d+1)):
+					if k%(d+1)==d:
+						stdout.buffer.write(bytes([result[k]]))
 
 
 	def ksa(self, t):
--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-from math import log, ceil
-from sys import argv
-
 from RC4 import RC4
 
 
-if len(argv) > 4:
-	n, l, d, t, mode = int(argv[1]), int(argv[2]), int(argv[3]), int(argv[1]), ''
-	if len(argv) == 5:
-		mode = argv[4]
-		t = ceil(2*n*log(n))
-	print(n, l, d, t, mode)
-	rc4 = RC4(l, n, mode, t)
-	print(rc4.mdrop(d, 10))
-else:
-	rc4 = RC4(40, 16, '', 16)
-	rc4.mdrop(0)
+rc4 = RC4(128, 256, '', 256)
+rc4.mdrop(0)
